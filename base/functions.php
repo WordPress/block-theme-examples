64f33425bcbcceae5b68666ec808cf097fd588cf
Justin Tadlock <justintadlock@gmail.com>
2023-10-30 15:46:39 -0500

    Add initial files and folders
    Gives a rough example of a base theme from to build from. There's still a few presets to sort out.

---
+++ base/functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Theme functions file, which is auto-loaded by WordPress. Use this file to
+ * load additional PHP files and bootstrap the theme.
+ *
+ * @author    Your Name <youremail@domain.tld>
+ * @copyright Copyright (c) 2023, Your Name
+ * @link      https://yourwebsite.tld
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
