5e56c99ce6c601b65f0d6d657f6aa6a32884d0d7
JuanMa Garrido <juanma.garrido@gmail.com>
2023-11-03 11:20:00 +0000

    createZip updated

--- bin/createZip.js
+++ bin/createZip.js
@@ -10,14 +10,37 @@ const createZipFromFolder = () => {
 
   const sourceFolderPath = path.join(rootPath, `${themeName}`)
   const destinationFolderPath = path.join(rootPath, "zips", `${themeExample}`)
+  const destinationFile = path.join(destinationFolderPath, `${themeName}.zip`)
   if (!fs.existsSync(destinationFolderPath)) {
     fs.mkdirSync(destinationFolderPath, { recursive: true });
   }
-  const output = fs.createWriteStream(path.join(destinationFolderPath, `${themeName}.zip`) )
+  const output = fs.createWriteStream( destinationFile )
 
   const archive = archiver('zip', {
       zlib: { level: 9 } // set compression level to the highest
   })
+
+
+  
+  output.on('close', function() {
+    console.log(`Created zips/${themeExample}/${themeName}.zip`);
+    process.exit(0);
+});
+
+  // good practice to catch warnings (ie stat failures and other non-blocking errors)
+  archive.on('warning', function(err) {
+    if (err.code === 'ENOENT') {
+      // log warning
+    } else {
+      // throw error
+      throw err;
+    }
+  });
+  
+  // good practice to catch this error explicitly
+  archive.on('error', function(err) {
+    throw err;
+  });
 
   archive.pipe(output)
   archive.directory(sourceFolderPath, false)
