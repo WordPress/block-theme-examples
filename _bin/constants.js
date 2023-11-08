0477f3c2b3e0877270e5d12256b13a4f42d7d05d
JuanMa Garrido <juanma.garrido@gmail.com>
2023-11-08 15:25:39 +0000

    updated playground link

--- _bin/constants.js
+++ _bin/constants.js
@@ -3,7 +3,7 @@ const SLUG_EXAMPLE_MARKER = "<%SLUG_EXAMPLE_MARKER%>";
 const URL_EXAMPLE_BASE = `https://raw.githubusercontent.com/${REPO_ORIGIN}/master/zips/base.zip`;
 const URL_EXAMPLE_THEME_ZIP = `https://raw.githubusercontent.com/${REPO_ORIGIN}/master/_zips/${SLUG_EXAMPLE_MARKER}.zip`;
 const URL_REPO = `https://github.com/${REPO_ORIGIN}/tree/master`
-const PLAYGROUND_URL_WITH_THEMES = `https://playground.wordpress.net/#{%22$schema%22:%22https://playground.wordpress.net/blueprint-schema.json%22,%22landingPage%22:%22/wp-admin/themes.php%22,%22preferredVersions%22:{%22php%22:%228.0%22,%22wp%22:%226.4-RC4%22},%22steps%22:[{%22step%22:%22installTheme%22,%22themeZipFile%22:{%22resource%22:%22wordpress.org/themes%22,%22slug%22:%22twentytwentyfour%22}},{%22step%22:%22installTheme%22,%22themeZipFile%22:{%22resource%22:%22url%22,%22url%22:%22${URL_EXAMPLE_THEME_ZIP}%22},%22options%22:{%22activate%22:true}},{%22step%22:%22login%22,%22username%22:%22admin%22,%22password%22:%22password%22}]}`
+const PLAYGROUND_URL_WITH_THEMES = `https://playground.wordpress.net/#{%22$schema%22:%22https://playground.wordpress.net/blueprint-schema.json%22,%22landingPage%22:%22/wp-admin/themes.php%22,%22preferredVersions%22:{%22php%22:%228.0%22,%22wp%22:%22latest%22},%22steps%22:[{%22step%22:%22installTheme%22,%22themeZipFile%22:{%22resource%22:%22wordpress.org/themes%22,%22slug%22:%22twentytwentyfour%22}},{%22step%22:%22installTheme%22,%22themeZipFile%22:{%22resource%22:%22url%22,%22url%22:%22${URL_EXAMPLE_THEME_ZIP}%22},%22options%22:{%22activate%22:true}},{%22step%22:%22login%22,%22username%22:%22admin%22,%22password%22:%22password%22}]}`
 
 module.exports = {
     REPO_ORIGIN,
