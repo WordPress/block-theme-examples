709f068d9b2821452542a5ef2a1fca8de9c41c08
JuanMa Garrido <juanma.garrido@gmail.com>
2023-12-07 15:58:39 -0500

    Updated tables with labels

--- _bin/constants.js
+++ _bin/constants.js
@@ -5,6 +5,8 @@ const URL_EXAMPLE_THEME_ZIP = `https://raw.githubusercontent.com/${REPO_ORIGIN}/
 const URL_REPO = `https://github.com/${REPO_ORIGIN}/tree/master`
 const PLAYGROUND_URL_WITH_THEMES = `https://playground.wordpress.net/#{%22$schema%22:%22https://playground.wordpress.net/blueprint-schema.json%22,%22landingPage%22:%22/wp-admin/themes.php%22,%22preferredVersions%22:{%22php%22:%228.0%22,%22wp%22:%22latest%22},%22steps%22:[{%22step%22:%22installTheme%22,%22themeZipFile%22:{%22resource%22:%22wordpress.org/themes%22,%22slug%22:%22twentytwentyfour%22}},{%22step%22:%22installTheme%22,%22themeZipFile%22:{%22resource%22:%22url%22,%22url%22:%22${URL_EXAMPLE_THEME_ZIP}%22},%22options%22:{%22activate%22:true}},{%22step%22:%22login%22,%22username%22:%22admin%22,%22password%22:%22password%22}]}`
 const URL_PLAYGROUND_BLUEPRINT = `https://raw.githubusercontent.com/${REPO_ORIGIN}/master/${SLUG_EXAMPLE_MARKER}/_playground/blueprint.json`
+const URL_WIKI = "https://github.com/${REPO_ORIGIN}/wiki"
+const WIKI_PAGE_TAGS = "Tags"
 
 module.exports = {
     REPO_ORIGIN,
@@ -13,5 +15,7 @@ module.exports = {
     URL_EXAMPLE_THEME_ZIP,
     PLAYGROUND_URL_WITH_THEMES,
     SLUG_EXAMPLE_MARKER,
-    URL_PLAYGROUND_BLUEPRINT
+    URL_PLAYGROUND_BLUEPRINT,
+    URL_WIKI,
+    WIKI_PAGE_TAGS,
 }
--- _bin/generateExamplesTableMarkdown.js
+++ _bin/generateExamplesTableMarkdown.js
@@ -6,6 +6,7 @@ const { info, error } = require("./utils/log");
 const rootPath = process.cwd();
 const readmePathRoot = join(rootPath, "README.md");
 const examplesJsonPath = join(rootPath, "_bin/data/examples.json");
+const tagsJsonPath = join(rootPath, "_bin/data/tags.json");
 
 const {
   URL_EXAMPLE_THEME_ZIP,
@@ -13,7 +14,9 @@ const {
   SLUG_EXAMPLE_MARKER,
   URL_REPO,
   REPO_ORIGIN,
-  URL_PLAYGROUND_BLUEPRINT
+  URL_PLAYGROUND_BLUEPRINT,
+  URL_WIKI,
+  WIKI_PAGE_TAGS,
 } = require("./constants");  
 
 const startMarker = "<!-- @TABLE EXAMPLES BEGIN -->";
@@ -22,11 +25,17 @@ const endMarker = "<!-- @TABLE EXAMPLES END -->";
 module.exports = ({ slug: slugReadme, readmePath = readmePathRoot }) => {
   
   const examplesJson = JSON.parse(fs.readFileSync(examplesJsonPath, "utf8"));
+  const tagsJson = JSON.parse(fs.readFileSync(tagsJsonPath, "utf8"));
   const markdownContent = fs.readFileSync(readmePath, "utf8");
     
   const regex = new RegExp(
     `${startMarker}\(\[\.\\n\\s\\S\]\*\)${endMarker}`,
     "gm"
+  );
+
+  const processedTags = tagsJson.reduce(
+    (acc, { slug, name }) => ({ ...acc, [slug]: name }),
+    {}
   );
 
   let processedExamplesJson;
@@ -37,7 +46,7 @@ module.exports = ({ slug: slugReadme, readmePath = readmePathRoot }) => {
     processedExamplesJson = examplesJson//.sort(sortFeaturedFirst);
   }  
 
-  const markdownTableRows = processedExamplesJson.map(({ slug, description }) => {
+  const markdownTableRows = processedExamplesJson.map(({ slug, description, tags }) => {
 
     let playgroundUrl = PLAYGROUND_URL_WITH_THEMES.replaceAll(SLUG_EXAMPLE_MARKER,slug);
     const urlZip = URL_EXAMPLE_THEME_ZIP.replaceAll(SLUG_EXAMPLE_MARKER,slug);
@@ -55,12 +64,15 @@ module.exports = ({ slug: slugReadme, readmePath = readmePathRoot }) => {
     return [
       `[📁](${URL_REPO}/${slug})`,
       description,
+      tags
+        .map((tagSlug) => `<small><code><a href="${URL_WIKI}/${WIKI_PAGE_TAGS}#${tagSlug}">${processedTags[tagSlug]}</a></code></small>`)
+        .join(", "),
       `[📦](${urlZip})`,
       `[![](${urlAssetIconWp})](${playgroundUrl})`,
     ];
   });
   const markdownTable = toMarkdownTable([
-    ["Folder", `Short description`, "Download .zip", "Live Demo"],
+    ["Folder", `Short description`, "Tags", "Download .zip", "Live Demo"],
     ...markdownTableRows,
   ]);
 
