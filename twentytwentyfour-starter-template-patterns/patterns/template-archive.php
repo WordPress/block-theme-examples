460085271b4daf28fbe436e44f74f3ff530d9ecd
Justin Tadlock <justintadlock@gmail.com>
2024-01-05 15:25:52 -0600

    Add starter template pattern example
    Partial fix for: https://github.com/WordPress/block-theme-examples/issues/7
    
    This adds an Twenty Twenty-Four child theme that showcases a starter archive template.

---
+++ twentytwentyfour-starter-template-patterns/patterns/template-archive.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Archive Template
+ * Slug: themeslug/template-archive
+ * Inserter: no
+ * Categories: featured, text
+ * Template Types: archive, author, date, category, taxonomy, tag
+ * Viewport Width: 1376
+ */
+?>
+<!-- wp:template-part {"slug":"header","area":"header","tagName":"header"} /-->
+
+<!-- wp:group {"tagName":"main","align":"full","layout":{"type":"constrained"}} -->
+<main class="wp-block-group alignfull">
+
+	<!-- wp:query-title {"type":"archive","align":"wide","style":{"typography":{"lineHeight":"1"},"spacing":{"padding":{"top":"var:preset|spacing|50"}}}} /-->
+
+	<!-- wp:pattern {"slug":"twentytwentyfour/posts-grid-2-col"} /-->
+
+</main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","area":"footer","tagName":"footer"} /-->
